2df1b79ead62a886ee4d785f5bb80c419c00612f
Katie Ford <katie@katieford.io>
2022-12-02 14:58:19 -0600

    day 2 part 2 confirmed correct answer

--- 2022/python/day2.py
+++ 2022/python/day2.py
@@ -48,28 +48,36 @@ follow the strategy guide, you would get a total score of 15 (8 + 1 + 6).
 
 What would your total score be if everything goes exactly according to your
 strategy guide?
-"""
-guide_translation = {
-    'A': 'rock',
-    'B': 'paper',
-    'C': 'scissors',
-    'X': 'rock',
-    'Y': 'paper',
-    'Z': 'scissors'
-}
 
-winner_to_loser = {
-    'rock': 'scissors',
-    'paper': 'rock',
-    'scissors': 'paper'
-}
+--- Part Two ---
+
+The Elf finishes helping with the tent and sneaks back over to
+you. "Anyway, the second column says how the round needs to end: X means you
+need to lose, Y means you need to end the round in a draw, and Z means you need
+to win. Good luck!"
+
+The total score is still calculated in the same way, but now you need to figure
+out what shape to choose so the round ends as indicated. The example above now
+goes like this:
+
+In the first round, your opponent will choose Rock (A), and you need the round
+to end in a draw (Y), so you also choose Rock. This gives you a score of 1 + 3
+= 4.  In the second round, your opponent will choose Paper (B), and you choose
+Rock so you lose (X) with a score of 1 + 0 = 1.  In the third round, you will
+defeat your opponent's Scissors with Rock for a score of 1 + 6 = 7.  Now that
+you're correctly decrypting the ultra top secret strategy guide, you would get
+a total score of 12.
+
+Following the Elf's instructions for the second column, what would your total
+score be if everything goes exactly according to your strategy guide?
+"""
 
 def determine_total_score(game_plays):
     elf_points = 0
     your_points = 0
     for play in game_plays:
         # each play is a tuple of theirs : yours
-        round_points = determine_round_points(play[0], play[1])
+        round_points = determine_new_round_points(play)
         elf_points += round_points[0]
         your_points += round_points[1]
     return your_points
@@ -77,6 +85,11 @@ def determine_total_score(game_plays):
 def determine_round_points(elf, you):
     # elf is the elf's play
     # you is your play
+    winner_to_loser = {
+        'rock': 'scissors',
+        'paper': 'rock',
+        'scissors': 'paper'
+    }
     play_scores = {'rock': 1, 'paper': 2, 'scissors': 3}
 
     your_points = play_scores[you]
@@ -90,6 +103,15 @@ def determine_round_points(elf, you):
         return (0 + play_scores[elf], 6 + play_scores[you])
 
 def process_guide():
+    guide_translation = {
+        'A': 'rock',
+        'B': 'paper',
+        'C': 'scissors',
+        'X': 'rock',
+        'Y': 'paper',
+        'Z': 'scissors'
+    }
+
     game_plays = []
     with open('data/day2.txt') as f:
         for line in f.readlines():
@@ -99,15 +121,60 @@ def process_guide():
             game_plays.append((guide_translation[plays[0]], guide_translation[plays[1]]))
     return game_plays
 
+def determine_new_round_points(play):
+    winner_to_loser = {
+        'rock': 'scissors',
+        'paper': 'rock',
+        'scissors': 'paper'
+    }
+    loser_to_winner = {v: k for k, v in winner_to_loser.items()}
+    play_scores = {'rock': 1, 'paper': 2, 'scissors': 3}
+
+    elf = play[0]
+    outcome = play[1]
+    you = ''
+
+    if outcome == 'draw':
+        you = elf
+        return (3 + play_scores[elf], 3 + play_scores[you])
+    elif outcome == 'win':
+        you = loser_to_winner[elf]
+        return (0 + play_scores[elf], 6 + play_scores[you])
+    else:
+        you = winner_to_loser[elf]
+        return (6 + play_scores[elf], 0 + play_scores[you])
+
+def process_updated_guide():
+    elf_play_translation = {
+        'A': 'rock',
+        'B': 'paper',
+        'C': 'scissors',
+    }
+
+    outcomes = {
+        'X': 'lose',
+        'Y': 'draw',
+        'Z': 'win'
+    }
+
+    game_plays = []
+    with open('data/day2.txt') as f:
+        # not super efficient because we cycle over the list twice but I can at
+        # least read this can update if necessary
+        for line in f.readlines():
+            game_round = line.strip().split(' ')
+            game_plays.append((elf_play_translation[game_round[0]], outcomes[game_round[1]]))
+    return game_plays
+
 def test_example():
-    game_plays = [('rock', 'paper'), ('paper', 'rock'), ('scissors', 'scissors')]
+    game_plays = [('rock', 'draw'), ('paper', 'lose'), ('scissors', 'win')]
     final_score = determine_total_score(game_plays)
-    if final_score == 15:
+    if final_score == 12:
         print("test works\n\n")
 
 if __name__ == '__main__':
     test_example()
 
     print("Puzzle answer:")
-    game_plays = process_guide()
+    game_plays = process_updated_guide()
     print(determine_total_score(game_plays))
