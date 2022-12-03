7a8c393d070aea93cfe10c6d54acedada4a51c3d
Katie Ford <katie@katieford.io>
2022-12-03 17:58:16 -0600

    day 3 reorg

--- 2022/node/day3.js
+++ 2022/node/day3.js
@@ -51,6 +51,51 @@
 
 	Find the item type that appears in both compartments of each rucksack. What
 	is the sum of the priorities of those item types?
+
+	--- Part Two ---
+
+	As you finish identifying the misplaced items, the Elves come to you with
+	another issue.
+
+	For safety, the Elves are divided into groups of three. Every Elf carries a
+	badge that identifies their group. For efficiency, within each group of three
+	Elves, the badge is the only item type carried by all three Elves. That is, if
+	a group's badge is item type B, then all three Elves will have item type B
+	somewhere in their rucksack, and at most two of the Elves will be carrying any
+	other item type.
+
+	The problem is that someone forgot to put this year's updated authenticity
+	sticker on the badges. All of the badges need to be pulled out of the rucksacks
+	so the new authenticity stickers can be attached.
+
+	Additionally, nobody wrote down which item type corresponds to each group's
+	badges. The only way to tell which item type is the right one is by finding the
+	one item type that is common between all three Elves in each group.
+
+	Every set of three lines in your list corresponds to a single group, but each
+	group can have a different badge item type. So, in the above example, the first
+	group's rucksacks are the first three lines:
+
+	vJrwpWtwJgWrhcsFMMfFFhFp
+	jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+	PmmdzqPrVvPwwTWBwg
+
+	And the second group's rucksacks are the next three lines:
+
+	wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+	ttgJtRGJQctTZtZT
+	CrZsJsPPZsGzwwsLwLmpwMDw
+
+	In the first group, the only item type that appears in all three rucksacks is
+	lowercase r; this must be their badges. In the second group, their badge item
+	type must be Z.
+
+	Priorities for these items must still be found to organize the sticker
+	attachment efforts: here, they are 18 (r) for the first group and 52 (Z) for
+	the second group. The sum of these is 70.
+
+	Find the item type that corresponds to the badges of each three-Elf group. What
+	is the sum of the priorities of those item types?
  */
 const cowsay = require('cowsay');
 const fs = require('fs');
@@ -59,15 +104,21 @@ const readline = require('readline');
 const testData = "vJrwpWtwJgWrhcsFMMfFFhFp\njqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL\n" +
 				  "PmmdzqPrVvPwwTWBwg\nwMqvLMZHhHMvwLHjbvcjnnSBnvTQFn\n" +
                   "ttgJtRGJQctTZtZT\nCrZsJsPPZsGzwwsLwLmpwMDw";
+const priorities = buildPriorities();
 
 function readInput() {
 	// read data from a file
     //const fileStream = fs.createReadStream('data/day3.txt');
 	const elfSacksRaw = fs.readFileSync('data/day3.txt', 'utf8');
 	//const elfSacksRaw = testData;
-	const priorities = buildPriorities();
 
 	const elfSacks = elfSacksRaw.split('\n');
+
+	// part 1 solution
+	part1Solution(elfSacks);
+}
+
+function part1Solution(elfSacks) {
 	let priorityPoints = 0;
 	elfSacks.forEach(sack => {
 		if (sack.length === 0) {
@@ -75,23 +126,31 @@ function readInput() {
 			// like not
 			return;
 		}
-		// breakdown the compartments
-		const midpoint = sack.length / 2;
-		// best to convert to array first since will need to convert to array anyways
-		sackItems = sack.split('');
-		const compartment1 = new Set(sackItems.slice(0, midpoint));
-		const compartment2 = new Set(sackItems.slice(midpoint));
 
 		// problem says we can assume that there is a single intersection of the sets
-		let sharedItem = '';
-		for (const item of compartment1) {
-			if (compartment2.has(item)) {
-				sharedItem = item;
-			}
-		}
+		const sharedItem = findMismatch(sack);
+
 		priorityPoints += priorities[sharedItem];
 	});
 	console.log(priorityPoints);
+}
+
+function findMismatch(sack) {
+	// breakdown the compartments
+	const midpoint = sack.length / 2;
+	// best to convert to array first since will need to convert to array anyways
+	sackItems = sack.split('');
+	const compartment1 = new Set(sackItems.slice(0, midpoint));
+	const compartment2 = new Set(sackItems.slice(midpoint));
+
+	// problem says we can assume that there is a single intersection of the sets
+	let sharedItem = '';
+	for (const item of compartment1) {
+		if (compartment2.has(item)) {
+			sharedItem = item;
+		}
+	}
+	return sharedItem;
 }
 
 function buildPriorities() {
