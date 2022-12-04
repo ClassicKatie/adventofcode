4b1a1d4b01f56d4a2cf8cdded6ade40be42e1ed9
Katie Ford <katie@katieford.io>
2022-12-04 15:43:12 -0600

    day 4 part 2

--- 2022/node/day4.js
+++ 2022/node/day4.js
@@ -59,6 +59,24 @@
 	are 2 such pairs.
 
 	In how many assignment pairs does one range fully contain the other?
+
+		--- Part Two ---
+	It seems like there is still quite a bit of duplicate work planned.
+	Instead, the Elves would like to know the number of pairs that overlap at
+	all.
+
+	In the above example, the first two pairs (2-4,6-8 and 2-3,4-5) don't
+	overlap, while the remaining four pairs (5-7,7-9, 2-8,3-7, 6-6,4-6, and
+	2-6,4-8) do overlap:
+
+	5-7,7-9 overlaps in a single section, 7.
+	2-8,3-7 overlaps all of the sections 3 through 7.
+	6-6,4-6 overlaps in a single section, 6.
+	2-6,4-8 overlaps in sections 4, 5, and 6.
+	So, in this example, the number of overlapping assignment pairs is 4.
+
+	In how many assignment pairs do the ranges overlap?
+
 */
 
 const cowsay = require('cowsay');
@@ -69,8 +87,8 @@ const testAssignmentsRaw = "2-4,6-8\n2-3,4-5\n5-7,7-9\n" +
 	                       "2-8,3-7\n6-6,4-6\n2-6,4-8";
 function findFullyContained(assignmentsRaw) {
 	const assignments = [];
-	fullyContained = 0;
-	anyOverlap = 0;
+	let fullyContained = 0;
+	let anyOverlap = 0;
 	for (const assignment of assignmentsRaw.split('\n')) {
 		if (assignment.length === 0) {
 			continue;
@@ -80,11 +98,13 @@ function findFullyContained(assignmentsRaw) {
 		firstAssignments = getFullAssignments(assignmentPair[0]);
 		secondAssignments = getFullAssignments(assignmentPair[1]);
 
-		// debug note: both assignment sets look good
 
+		let hasOverlap = false;
 		// Now we have sets of assignments. Let's check the intersection
 		for (const section of firstAssignments) {
 			if (secondAssignments.has(section)) {
+				// we now know there is at least some overlap
+				hasOverlap = true;
 				// if they both have this section, we can remove it from both sets
 			    firstAssignments.delete(section);
 				secondAssignments.delete(section);
@@ -96,8 +116,12 @@ function findFullyContained(assignmentsRaw) {
 		if (firstAssignments.size == 0 || secondAssignments.size == 0) {
 			fullyContained++;
 		}
+		if (hasOverlap) {
+			anyOverlap++;
+		}
 	}
-	return fullyContained;
+	console.log('fully contained: ' + fullyContained);
+	console.log('have overlap: ' + anyOverlap);
 }
 
 function getFullAssignments(range) {
